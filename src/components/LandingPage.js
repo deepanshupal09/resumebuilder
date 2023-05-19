a98bb655522bc62d2ac6af1648ee03832e3349c6
Deepanshu <dp5114@dseu.ac.in>
2023-05-19 20:12:01 +0530

    change

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='bg-[#181716] h-[35vw] text-white'>
+      <div className='text-6xl font-bold font-bungee'>Craft Your Career Path with Precision</div>
+      <div>Your Perfect Resume Starts Here!</div>
+    </div>
+  )
+}
+
+export default LandingPage
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      bungee: [['Merriweather', 'serif'], ['Montserrat', 'sans-serif'] ,['Sacramento', 'cursive']]
+    },
     extend: {},
   },
   plugins: [],
