efae0cac990ad76c6220ee4702648469d66d9a97
anant <ab6157@dseu.ac.in>
2023-05-20 13:03:51 +0530

    not now 1

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,7 +6,7 @@ function LandingPage() {
      <div className=' mx-10'>
       <div className='relative top-[5vw] text-5xl font-bold font-sans'>Craft Your Career Path with Precision..</div>
       <div className='relative top-[6vw] text-4xl mt-4 font-bold font-sans'>Your Perfect Resume Starts Here!</div>
-      <div className='mx-2 flex mt-12 space-x-8 relative top-[14rem] '>
+      <div className='mx-2 flex mt-12 space-x-8 relative top-[8vw] '>
         <button className=' rounded-md  font-semibold shadow-lg px-5 text-lg py-3' style={{backgroundImage: 'linear-gradient(90deg,#e8b93f,#d68423)'}}>Start Now</button>
         <button className=' rounded-md  font-semibold shadow-lg px-5 text-lg py-3' style={{backgroundImage: 'linear-gradient(90deg,#e8b93f,#d68423)'}}>Click Here</button>
       </div>
