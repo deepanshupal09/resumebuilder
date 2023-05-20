6d3f98f03407005e3856dee8f988cd3b60cdd08c
Deepanshu <dp5114@dseu.ac.in>
2023-05-21 03:22:20 +0530

    made website responsive

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,6 +13,9 @@ function LandingPage() {
   const scrollLeft = () => {
     document.getElementById('temp').scrollLeft -= 1000;
   } 
+  
+  
+
   AOS.init();
   return (
     
@@ -20,30 +23,30 @@ function LandingPage() {
      
     <div className='first-component flex text-white h-[75vh]' style={{backgroundImage: 'linear-gradient( 90.1deg,  rgba(84,212,228,1) 0.2%, rgba(68,36,164,1) 99.9% )'}}>
      <div className=' mx-12 slideleft'>
-      <div className='relative top-[25%] text-5xl font-bold font-sans'>Craft Your Career Path with Precision..</div>
-      <div className='relative top-[32%] text-4xl mt-4 font-bold font-sans'>Your Perfect Resume Starts Here!</div>
+      <div className=' relative top-[25%] text-3xl lg:text-5xl font-bold font-sans'>Craft Your Career Path with Precision..</div>
+      <div className=' relative top-[32%] text-2xl lg:text-4xl mt-4 font-bold font-sans'>Your Perfect Resume Starts Here!</div>
       <div className='mx-2 flex mt-12 space-x-8 relative top-[40%] slideleft'>
         <button className=' rounded-md  font-semibold shadow-lg px-5 text-lg py-3 ' style={{backgroundImage: 'linear-gradient(90deg,#e8b93f,#d68423)'}}>Start Now</button>
         <button className=' rounded-md  font-semibold shadow-lg px-5 text-lg py-3 ]' style={{backgroundImage: 'linear-gradient(90deg,#e8b93f,#d68423)'}}>Click Here</button>
       </div>
      </div>
-     <div className='flex justify-end w-1/2 '><img className='mr-[10%] slideright h-[110%]' src="./resume-img.png" alt="" /></div>
+     <div className='md:flex hidden justify-end w-1/2 '><img className='mr-[10%] slideright h-[110%]' src="./resume-img.png" alt="" /></div>
     </div>
     <div data-aos="fade-right" className='second-component m-2 h-[100vh] py-[2%] px-[3%]' style={{marginTop: '25vh'}}>
         <h1 className='text-5xl relative top-[7vh] text-slate-600 text-center font-bold font-mons'>Templates For Resume</h1>
 
-        <div className='flex relative top-[10vh] min-h-[70vh] w-[90vw] mx-auto space-x-10'>
+        <div className='flex relative top-[10vh] h-[100vw] w-[95vw] overflow-x-hidden lg:h-[45vw] lg:w-[95vw]  mx-auto space-x-10'>
           <div className='my-auto'>
-            <button className='rounded-[50%] text-center p-4  hover:shadow-lg bg-white bg-opacity-0 hover:bg-opacity-40 text-2xl' onClick={scrollLeft}><FontAwesomeIcon icon={faChevronLeft} /></button>
+            <button className='rounded-[50%] text-center p-0 md:p-4 hidden lg:flex hover:shadow-lg bg-white bg-opacity-0 hover:bg-opacity-40 text-2xl' onClick={scrollLeft}><FontAwesomeIcon icon={faChevronLeft} /></button>
           </div>
-          <div className=' scroll-smooth template overflow-auto   mx-auto flex space-x-[5vw] mt-[5vh] flex-nowrap  overflow-x-hidden' id='temp'>
-            <div className='h-[65vh] ' style={{flex: "0 0 auto"}}> <img src="./resume-1.png" className='w-[500px] h-[100%]'alt="" /> </div>
-            <div className='h-[65vh] ' style={{flex: "0 0 auto"}}> <img src="./resume-2.png" className='w-[500px] h-[100%]' alt="" /> </div>
-            <div className='h-[65vh] ' style={{flex: "0 0 auto"}}> <img src="./resume-3.png" className='w-[500px] h-[100%]' alt="" /> </div>
-            <div className='h-[65vh] ' style={{flex: "0 0 auto"}}> <img src="./resume-4.png" className='w-[500px] h-[100%]' alt="" /> </div>
+          <div className=' scroll-smooth template   mx-auto flex space-x-[3vw] mt-[5vh] flex-nowrap overflow-y-visible overflow-x-hidden' id='temp'>
+            <div className='lg:w-[25vw] overflow-visible px-0 lg:px-2 py-4 lg:h-[35vw]' style={{flex: "0 0 auto"}}> <img src="./resume-1.jpeg" className='h-[100%] cursor-pointer hover:scale-[110%] transition-all duration-300  w-[100%] shadow-2xl'alt="" /> </div>
+            <div className='lg:w-[25vw] overflow-visible px-0 lg:px-2 py-4 lg:h-[35vw]' style={{flex: "0 0 auto"}}> <img src="./resume-2.jpeg" className='h-[100%] cursor-pointer hover:scale-[110%] transition-all duration-300  w-[100%] shadow-2xl' alt="" /> </div>
+            <div className='lg:w-[25vw] overflow-visible px-0 lg:px-2 py-4 lg:h-[35vw]' style={{flex: "0 0 auto"}}> <img src="./resume-3.jpeg" className='h-[100%] cursor-pointer hover:scale-[110%] transition-all duration-300  w-[100%] shadow-2xl' alt="" /> </div>
+            <div className='lg:w-[25vw] overflow-visible px-0 lg:px-2 py-4 lg:h-[35vw]' style={{flex: "0 0 auto"}}> <img src="./resume-4.jpeg" className='h-[100%]  cursor-pointer hover:scale-[110%] transition-all duration-300 w-[100%] shadow-2xl' alt="" /> </div>
           </div>
           <div className='my-auto'>
-            <button className='rounded-[50%] text-center p-4   hover:shadow-lg bg-white bg-opacity-0 hover:bg-opacity-40 text-2xl' onClick={scrollRight}><FontAwesomeIcon icon={faChevronRight} /></button>
+            <button className='rounded-[50%] text-center p-0 md:p-4 hidden lg:flex  hover:shadow-lg bg-white bg-opacity-0 hover:bg-opacity-40 text-2xl' onClick={scrollRight}><FontAwesomeIcon icon={faChevronRight} /></button>
           </div>
         </div>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 function Navbar() {
+
+  const [open, setOpen] = useState('false');
+
+  const showMenu = () => {
+    if (open === 'false') {
+      console.log(open);
+      document.getElementById('menu').style.height = '100vh';
+      setOpen('true');
+    } else {
+      console.log(open);
+      document.getElementById('menu').style.height = '0';
+      setOpen('false');
+    }
+  }
 
   const test = () => {
     var a = window.getComputedStyle( document.body ,null).getPropertyValue('background-color'); 
@@ -13,10 +29,10 @@ function Navbar() {
   }
 
   return (
-
+<>
     <div>
       {window.onscroll = test}
-      <div id='nav' className=" shadow-2xl fixed top-0  backdrop-blur-3xl backdrop-filter bg-opacity-30 z-10 w-[100%] flex justify-between h-[7vh] bg-black text-lg  text-white" >
+      <div id='nav' className=" shadow-2xl fixed top-0 justify backdrop-blur-3xl backdrop-filter bg-opacity-30 z-10 w-[100%] flex justify-between h-[7vh] bg-black text-lg  text-white" >
         <div className="my-auto"> 
           <img
             className="w-[70%]"
@@ -24,7 +40,13 @@ function Navbar() {
             alt="Resume Builder"
           />
         </div>
-        <ul className=" flex space-x-11 mr-16 my-auto ">
+        <div className="lg:hidden text-2xl w-[10vw] flex items-center align-baseline justify-center cursor-pointer">
+            <button onClick={showMenu}>
+             <FontAwesomeIcon icon={faBars} /> </button>
+        </div>
+
+        
+        <ul className="hidden lg:flex  space-x-11 mr-16 my-auto ">
         <div className="">
          
           <button className=" px-2 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-md py-2 " >
@@ -50,6 +72,14 @@ function Navbar() {
         </ul>
       </div>
     </div>
+    <div className="flex lg:hidden flex-col divide-y-0 text-xl overflow-hidden text-white w-[100%] h-[0] fixed top-[7vh] z-10 transition-all duration-500 bg-black backdrop-blur-3xl backdrop-filter bg-opacity-30 "  id='menu'>
+      <div className="py-4 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-md px-10 mt-10 ">Build Your Resume</div>
+      <div className="py-4 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-md px-10">FAQ</div>
+      <div className="py-4 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-md px-10">About Us</div>
+      <div className="py-4 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-md px-10">Login</div>
+      <div className="py-4 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-md px-10">Sign up</div>
+    </div>
+    </>
   );
 }
 
